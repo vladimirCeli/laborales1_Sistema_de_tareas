097012849fc26f9f19d7c397cdb407a00995bae1
vladimirCeli <vladimir.celi@unl.edu.ec>
2022-03-15 17:27:25 -0500

    Cambios

--- routes/Student.js
+++ routes/Student.js
@@ -4,18 +4,18 @@ const router = express.Router();
 
 const conexion = require("../database/db");
 
-router.get("/vizpropslist/:estudianteId", (req, res) => {
+router.get("/lista_propuestas_tareas/:estudianteId", (req, res) => {
     const estudianteId = req.params.estudianteId;
     conexion.query("SELECT * FROM tareas WHERE estudianteId = ? AND docenteId is null", [estudianteId], (error, results) => {
         if (error) {
             throw error;
         }else {
-            res.render("vizpropslist", {results: results, header: 'partials/_header'});
+            res.render("lista_propuestas_tareas", {results: results, header: 'partials/_header', headerDos: 'partials/headerPersonal'});
         }
     });
 });
 
-router.get("/listpropustareas/:id", (req, res) => {
+router.get("/propuesta_tarea/:id", (req, res) => {
     const id = req.params.id;
     conexion.query("SELECT * FROM propuesta WHERE tareas3 IN (SELECT id FROM tareas WHERE id = ?)", [id], (error, results) => {
         if (error) {
@@ -26,7 +26,7 @@ router.get("/listpropustareas/:id", (req, res) => {
                     throw error;
                 } else {
                     conexion.query("SELECT * FROM especialidades WHERE docentes IN (SELECT id FROM docentes WHERE id IN (SELECT docente FROM propuesta where tareas3 IN (SELECT id From tareas Where id = ?)))", [id], (error, re) => {
-                        res.render('listpropustareas', {results: results, resul: resul, re: re, header: 'partials/_header'});
+                        res.render('propuesta_tarea', {results: results, resul: resul, re: re, header: 'partials/_header', headerDos: 'partials/headerPersonal'});
                     });
                 }
             });
--- routes/Teacher.js
+++ routes/Teacher.js
@@ -4,36 +4,36 @@ const router = express.Router();
 
 const conexion = require("../database/db");
 
-router.get("/busqdalista", (req, res) => {
+router.get("/principal_busqueda_tareas", (req, res) => {
     conexion.query("SELECT * FROM tareas WHERE docenteId is NULL", (error, results) => {
         if (error) {
             throw error;
         } else {
-            res.render("busqdalista", {results: results, header: 'partials/_header'});
+            res.render("principal_busqueda_tareas", {results: results, header: 'partials/_header', headerDos: 'partials/headerPersonal'});
     }
     });
 });
 
-router.post("/busqdalista", (req, res) => {
+router.post("/principal_busqueda_tareas", (req, res) => {
     let searchTerm = req.body.search;
     conexion.query("SELECT * FROM tareas WHERE docenteId is NULL AND titulo LIKE ?", ['%' + searchTerm + '%'], (error, results) => {
         //conexion.release();
         if (error) {
             throw error;
         } else {
-            res.render("busqdalista", {results: results, header: 'partials/_header'});
+            res.render("principal_busqueda_tareas", {results: results, header: 'partials/_header', headerDos: 'partials/headerPersonal'});
     }
     });
 });
 
 
-router.get("/subitarcom/:docenteId", (req, res) => {
+router.get("/completar_tarea/:docenteId", (req, res) => {
     const docenteId = req.params.docenteId;
     conexion.query("SELECT * FROM tareas WHERE docenteId = ?", [docenteId], (error, results) => {
         if (error) {
             throw error;
         } else {
-            res.render('subitarcom', {results: results, header: 'partials/_header'});
+            res.render('completar_tarea', {results: results, header: 'partials/_header', headerDos: 'partials/headerPersonal'});
         }
     });
 });
